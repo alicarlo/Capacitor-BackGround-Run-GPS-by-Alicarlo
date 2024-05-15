37f813286e354e7a9d6d46d80a839b443b7ac64a
alicarlo <alicarlo.montijop@gmail.com>
2024-05-15 15:04:08 -0700

    check

---
+++ dist/esm/web.d.ts
@@ -0,0 +1,37 @@
+import { PluginListenerHandle, WebPlugin } from '@capacitor/core';
+import type { backgroundrunPlugin, GpsOptions } from './definitions';
+export declare class backgroundrunWeb extends WebPlugin implements backgroundrunPlugin {
+    echo(options: {
+        value: string;
+    }): Promise<{
+        value: string;
+    }>;
+    showNotificationOnAppClose(options: GpsOptions): Promise<GpsOptions>;
+    stopNotificationService(): Promise<void>;
+    checkNotificationPermission(): Promise<void>;
+    openNotificationSettings(): Promise<void>;
+    checkPermissionsService(options: {
+        value: boolean;
+    }): Promise<{
+        value: boolean;
+    }>;
+    ignoringBatteryOptimizationsService(): Promise<void>;
+    openLocationSettings(): Promise<void>;
+    addAppResumedListener(): Promise<void>;
+    addListener(eventName: string, listenerFunc: (...args: any[]) => void): Promise<PluginListenerHandle>;
+    clearAppResumedListener(): Promise<void>;
+    requestBatteryOptimizations(): Promise<void>;
+    requestNotificationPermission(): Promise<void>;
+    checkUsageStatsNotificationPausePermission(options: {
+        value: boolean;
+    }): Promise<{
+        value: boolean;
+    }>;
+    checkManageAppPermissionsPermission(options: {
+        value: boolean;
+    }): Promise<{
+        value: boolean;
+    }>;
+    acquireWakeLock(): Promise<void>;
+    releaseWakeLock(): Promise<void>;
+}
---
+++ dist/plugin.js
@@ -0,0 +1,97 @@
+var capacitorbackgroundrun = (function (exports, core) {
+    'use strict';
+
+    const backgroundrun = core.registerPlugin('backgroundrun', {
+        web: () => Promise.resolve().then(function () { return web; }).then(m => new m.backgroundrunWeb()),
+    });
+
+    class backgroundrunWeb extends core.WebPlugin {
+        async echo(options) {
+            console.log('ECHO', options);
+            return options;
+        }
+        async showNotificationOnAppClose(options) {
+            return options;
+        }
+        async stopNotificationService() {
+            throw new Error('Method not implemented.');
+            // Implementa aquí la lógica para mostrar la notificación en la web
+        }
+        async checkNotificationPermission() {
+            throw new Error('Method not implemented.');
+            // Implementa aquí la lógica para mostrar la notificación en la web
+        }
+        async openNotificationSettings() {
+            throw new Error('Method not implemented.');
+            // Implementa aquí la lógica para mostrar la notificación en la web
+        }
+        async checkPermissionsService(options) {
+            return options;
+        }
+        async ignoringBatteryOptimizationsService() {
+            throw new Error('Method not implemented.');
+            // Implementa aquí la lógica para mostrar la notificación en la web
+        }
+        async openLocationSettings() {
+            throw new Error('Method not implemented.');
+            // Implementa aquí la lógica para mostrar la notificación en la web
+        }
+        /*async addAppResumedListener(callback: () => void): Promise<void> {
+            callback();
+        }*/
+        async addAppResumedListener() {
+            window.addEventListener('appResumed', () => {
+                this.notifyListeners('appResumed', {});
+            });
+            return Promise.resolve();
+        }
+        async addListener(eventName, listenerFunc) {
+            const pluginListenerHandle = {
+                remove: () => {
+                    window.removeEventListener(eventName, listenerFunc);
+                    return Promise.resolve();
+                }
+            };
+            return Promise.resolve(pluginListenerHandle);
+        }
+        async clearAppResumedListener() {
+            throw new Error('Method not implemented.');
+            // Implementa aquí la lógica para mostrar la notificación en la web
+        }
+        async requestBatteryOptimizations() {
+            throw new Error('Method not implemented.');
+            // Implementa aquí la lógica para mostrar la notificación en la web
+        }
+        async requestNotificationPermission() {
+            throw new Error('Method not implemented.');
+            // Implementa aquí la lógica para mostrar la notificación en la web
+        }
+        async checkUsageStatsNotificationPausePermission(options) {
+            return options;
+        }
+        async checkManageAppPermissionsPermission(options) {
+            return options;
+        }
+        async acquireWakeLock() {
+            throw new Error('Method not implemented.');
+            // Implementa aquí la lógica para mostrar la notificación en la web
+        }
+        async releaseWakeLock() {
+            throw new Error('Method not implemented.');
+            // Implementa aquí la lógica para mostrar la notificación en la web
+        }
+    }
+
+    var web = /*#__PURE__*/Object.freeze({
+        __proto__: null,
+        backgroundrunWeb: backgroundrunWeb
+    });
+
+    exports.backgroundrun = backgroundrun;
+
+    Object.defineProperty(exports, '__esModule', { value: true });
+
+    return exports;
+
+})({}, capacitorExports);
+//# sourceMappingURL=plugin.js.map
