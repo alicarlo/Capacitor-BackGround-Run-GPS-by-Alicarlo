1732b4d4dcd3ad622e10a4ad9a4982c358836530
alicarlo <alicarlo.montijop@gmail.com>
2024-05-15 13:48:48 -0700

    check

--- dist/esm/index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { backgroundrunPlugin } from './definitions';
-declare const backgroundrun: backgroundrunPlugin;
-export * from './definitions';
-export { backgroundrun };
--- dist/esm/index.js
+++
@@ -1,7 +0,0 @@
-import { registerPlugin } from '@capacitor/core';
-const backgroundrun = registerPlugin('backgroundrun', {
-    web: () => import('./web').then(m => new m.backgroundrunWeb()),
-});
-export * from './definitions';
-export { backgroundrun };
-//# sourceMappingURL=index.js.map
